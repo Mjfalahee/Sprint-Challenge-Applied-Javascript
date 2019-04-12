de9c3109b332145385d8b00b35bea924ced8227c
mjfalahee <mjfalahee@gmail.com>
2019-04-12 10:40:43 -0700

    Still working on stretch. Made the right button work properly, working on the left button

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -2,47 +2,78 @@ class Carousel {
     constructor(carousel) {
         this.carousel = carousel //this carousel is assigned to DOM ref
         this.leftbutton = carousel.querySelector('.left-button') // Same for leftbutton
-        console.log(this.leftbutton)
+        //console.log(this.leftbutton)
         this.rightbutton = carousel.querySelector('.right-button') // right button
-        console.log(this.rightbutton)
+        //console.log(this.rightbutton)
 
         // node list of images
         this.allimages = carousel.querySelectorAll('.carousel-img')
-        console.log(this.allimages)
+        //console.log(this.allimages)
         this.imageindex = Array.from(this.allimages)
-        console.log(this.imageindex)
+        //console.log(this.imageindex)
         this.currentimage = this.imageindex[0]
-        console.log(this.currentimage)
+        //console.log(this.currentimage)
 
         this.rightbutton.addEventListener('click', () => this.moveright())
         this.leftbutton.addEventListener('click', () => this.moveleft())
     }
 
     moveright() {
-        let x = this.currentimage
-        console.log(x)
-        if (this.currentimage === carousel.querySelector(`.carousel-img[data-tab='4']`)) {
-            this.currentimage = this.allimages[0]
-            console.log('current image set')
-            this.currentimage.style.display = 'flex'
+        let current = this.currentimage.dataset.number
+        let reset = this.imageindex[0]
+        console.log('current is: ' + current)
+
+        this.imageindex.forEach(element => {
+            element.classList.remove('active-img')
+        })
+
+        if (current != 4) {
+            this.currentimage = this.imageindex[current]
+            console.log(this.currentimage)
+            this.currentimage.classList.add('active-img')
         }
 
         else {
-            this.currentimage = this.currentimage
+            current = 0
+            this.currentimage = this.imageindex[current]
+            console.log(this.currentimage)
+            this.currentimage.classList.add('active-img')
         }
 
     }
 
     moveleft() {
+        let current = this.currentimage.dataset.number
+        console.log('current is: ' + current)
+
+        this.imageindex.forEach(element => {
+            element.classList.remove('active-img')
+        })
+
+        
+
+        // if (current != 1) {
+        //     this.currentimage = this.imageindex[(4-current)]
+        //     console.log(this.currentimage)
+        //     this.currentimage.classList.add('active-img')
+        // }
+
+        // else {
+        //     //current = 3
+        //     this.currentimage = this.imageindex[current]
+        //     console.log(this.currentimage)
+        //     this.currentimage.classList.add('active-img')
+        // }
+
 
     }
 
 }
 
 let carousel = document.querySelector('.carousel');
-console.log(carousel)
+//console.log(carousel)
 function CarouselBuilder(name) {
-    console.log('building Carousel')
+    //console.log('building Carousel')
     return new Carousel(name)
 }
 
